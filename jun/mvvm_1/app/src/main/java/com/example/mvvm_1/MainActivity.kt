51d6dec3740897eda7d1a7276a0378438ddef442
JunYeong Kim <96372487+JunYeong0314@users.noreply.github.com>
2023-09-21 11:47:47 +0900

    week1 미션

---
+++ jun/mvvm_1/app/src/main/java/com/example/mvvm_1/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mvvm_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvm_1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
